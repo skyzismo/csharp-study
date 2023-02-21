88bc5cd628a1a1be46776211820f6e45ae31cc24
skyzismo <lucas.petigato@hotmail.com>
2023-02-21 15:10:19 -0400

    first commit

---
+++ functions/functions/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main()
+    {
+        FnTypeA();
+        FnTypeB("função do tipo B\n");
+        Console.WriteLine($"Essa é uma função do tipo C, que retorna a soma de 2 e 4: {FnTypeC()}\n");
+        Console.WriteLine($"Essa é uma função do tipo D, que retorna o produto entre 2 e 4: {FnTypeD(2, 4)}");
+    }
+
+    // Tipos de Funções
+
+    private static void FnTypeA()
+    {
+        Console.WriteLine($"Essa é uma função do tipo A \n");
+    }
+
+    private static void FnTypeB(string nome)
+    {
+        Console.WriteLine($"Essa é uma {nome}");
+    }
+
+    private static int FnTypeC()
+    {
+        return 2+4;
+    }
+
+    private static int FnTypeD(int a, int b)
+    {
+        return a * b;
+    }
+}
---
+++ helloWorld/helloWorld/Program.cs
@@ -0,0 +1,54 @@
+﻿internal class Program
+{
+    /*
+    private static void Main()
+    {
+        Console.WriteLine("Digite seu nome e, em seguida, os números que deseja somar:\n");
+
+        string nome = Console.ReadLine()
+            .Trim();
+
+        int a = int.Parse(s: Console.ReadLine());
+
+        int b = int.Parse(s: Console.ReadLine());
+
+        Console.WriteLine($"\nOi, {nome}! A soma dos valores é {a + b}");
+    }
+    */
+
+    private static int Main()
+    {
+        // ponto flutuante
+
+        float f = 2.1f;
+        double d = 4.6d;
+        decimal m = 10.8m;
+
+        // atribuição implícita e constantes JAVASCRIPTU UHUUU
+
+        var idade = 20;
+
+        f = idade; // conversão implícita
+
+        const string cidade = "Manaus";
+
+        // conversão explícita
+        Console.WriteLine($"{(f*d*(float)m)}", idade, cidade);
+
+        bool tipo = f == d;
+
+        Console.WriteLine(tipo);
+
+        // Strings
+
+        string frase = " Meu nome é Luc@s Skyzismo ";
+
+        Console.WriteLine(frase.Trim().Replace("Lucas", "Sky").Substring(0, 14));
+
+        string cpf = "2345678900";
+
+        Console.WriteLine(cpf.PadLeft(11,'1'));
+
+        return idade;
+    }
+}
