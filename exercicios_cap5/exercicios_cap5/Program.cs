fe047bfa9a23ffec12f7ea2558781e3fd7b103cf
skyzismo <lucas.petigato@hotmail.com>
2023-02-22 18:43:44 -0400

    add exercecicios cap 05

---
+++ exercicios_cap5/exercicios_cap5/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("Exercícios Cap. 5\n");
+        Console.WriteLine(Questao_01());
+    }
+
+    private static string Questao_01()
+    {
+        string num = Console.ReadLine();
+        return num;
+    }
+}
--- functions/functions/Program.cs
+++ functions/functions/Program.cs
@@ -6,6 +6,7 @@
         FnTypeB("função do tipo B\n");
         Console.WriteLine($"Essa é uma função do tipo C, que retorna a soma de 2 e 4: {FnTypeC()}\n");
         Console.WriteLine($"Essa é uma função do tipo D, que retorna o produto entre 2 e 4: {FnTypeD(2, 4)}");
+        Menu();
     }
 
     // Tipos de Funções
@@ -28,5 +29,42 @@
     private static int FnTypeD(int a, int b)
     {
         return a * b;
+    }
+
+    private static void Menu()
+    {
+        Console.WriteLine("Escolha uma opção: a - Contar de 1 a 4, b - Contar de 6 a 9 ou c - Contar toda a lista\n");
+
+        string option = Console.ReadLine();
+
+        int[] array_1 = new int[] {1,2,3,4,5,6,7,8,9};
+
+        switch (option)
+        {
+            case "a":
+
+                for (int i = 0; i < array_1.Length - 4; i++)
+                {
+                    Console.WriteLine(array_1[i]);
+                }break;
+                
+            case "b":
+
+                for (int i = 5; i < array_1.Length; i++)
+                {
+                    Console.WriteLine(array_1[i]);
+                }break;
+
+            case "c":
+
+                foreach (int num in array_1)
+                {
+                    Console.WriteLine(num);
+                }break;
+
+            default:
+                Console.WriteLine("Opção inválida, tente novamente :)");
+                break;
+        }
     }
 }
